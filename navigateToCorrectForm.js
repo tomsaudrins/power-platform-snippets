a42d17afe4455e9b2da824788dbdcc6bbc287921
Toms Audrins <67427155+tomsaudrins@users.noreply.github.com>
2022-05-30 13:23:58 +0200

    Don't change form if IDs match

--- navigateToCorrectForm.js
+++ navigateToCorrectForm.js
@@ -4,8 +4,9 @@ function navigate(executionContext) {
     const serviceType = formContext.getAttribute("kontant_servicetype").getValue()
     const availableForms = formContext.ui.formSelector.items.get();
     const form = availableForms.filter(form => form._label == serviceType[0].name)
+    const formId = formContext.ui.formSelector.getCurrentItem().getId();
 
-    if (form.length)
+    if (form.length && form[0]._id.guid != formId)
         form[0].navigate()
     
 }
